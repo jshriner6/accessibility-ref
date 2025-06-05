9ad04ba1dc04c1c1afc75f22e86da9a70475e191
Jeff <110954699+jshriner6@users.noreply.github.com>
2025-06-05 22:39:34 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -1,13 +1,31 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
-  "id": "tool",
+  "id": "oer",
   "level": "1",
-  "url": "tool.html",
-  "type": "Article",
-  "number": "",
-  "title": "Accessibility Tools",
-  "body": " Accessibility Tools   General Remarks   PreTeXt  PreTeXt  PreTeXt  "
+  "url": "oer.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Open Source Textbooks",
+  "body": " Open Source Textbooks   Remarks about general use cases and level of time commitment.    Workflow 1   Some words    My First Subsection  Some more words  foo     Workflow 2   Some words    "
+},
+{
+  "id": "pretext",
+  "level": "1",
+  "url": "pretext.html",
+  "type": "Section",
+  "number": "2",
+  "title": "PreTeXt",
+  "body": " PreTeXt   Remarks about general use cases and level of time commitment.    Workflow 1   Some words    My First Subsection  Some more words  foo     Workflow 2   Some words    "
+},
+{
+  "id": "quarto",
+  "level": "1",
+  "url": "quarto.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Quarto",
+  "body": " Quarto   Remarks about general use cases and level of time commitment.    Workflow 1   Some words    My First Subsection  Some more words  foo     Workflow 2   Some words    "
 }
 ]
 

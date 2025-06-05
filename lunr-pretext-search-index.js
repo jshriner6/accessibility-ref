4246cebf5890ff32f1806082795027cf1c938fef
Jeff <110954699+jshriner6@users.noreply.github.com>
2025-06-05 22:48:51 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -26,6 +26,78 @@ var ptx_lunr_docs = [
   "number": "3",
   "title": "Quarto",
   "body": " Quarto   Remarks about general use cases and level of time commitment.    Workflow 1   Some words    My First Subsection  Some more words  foo     Workflow 2   Some words    "
+},
+{
+  "id": "latexml",
+  "level": "1",
+  "url": "latexml.html",
+  "type": "Section",
+  "number": "4",
+  "title": "LaTeXML",
+  "body": " LaTeXML   Remarks about general use cases and level of time commitment.    Workflow 1   Some words    My First Subsection  Some more words  foo     Workflow 2   Some words    "
+},
+{
+  "id": "canvas",
+  "level": "1",
+  "url": "canvas.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Canvas",
+  "body": " Canvas   Remarks about general use cases and level of time commitment.    Workflow 1   Some words    My First Subsection  Some more words  foo     Workflow 2   Some words    "
+},
+{
+  "id": "ai",
+  "level": "1",
+  "url": "ai.html",
+  "type": "Section",
+  "number": "6",
+  "title": "AI",
+  "body": " AI   Remarks about general use cases and level of time commitment.    Workflow 1   Some words    My First Subsection  Some more words  foo     Workflow 2   Some words    "
+},
+{
+  "id": "prefigure",
+  "level": "1",
+  "url": "prefigure.html",
+  "type": "Section",
+  "number": "7",
+  "title": "PreFigure",
+  "body": " PreFigure   Remarks about general use cases and level of time commitment.    Workflow 1   Some words    My First Subsection  Some more words  foo     Workflow 2   Some words    "
+},
+{
+  "id": "webwork",
+  "level": "1",
+  "url": "webwork.html",
+  "type": "Section",
+  "number": "8",
+  "title": "WeBWorK",
+  "body": " WeBWorK   Remarks about general use cases and level of time commitment.    Workflow 1   Some words    My First Subsection  Some more words  foo     Workflow 2   Some words    "
+},
+{
+  "id": "desmos",
+  "level": "1",
+  "url": "desmos.html",
+  "type": "Section",
+  "number": "9",
+  "title": "Desmos",
+  "body": " Desmos   Remarks about general use cases and level of time commitment.    Workflow 1   Some words    My First Subsection  Some more words  foo     Workflow 2   Some words    "
+},
+{
+  "id": "doenet",
+  "level": "1",
+  "url": "doenet.html",
+  "type": "Section",
+  "number": "10",
+  "title": "Doenet",
+  "body": " Doenet   Remarks about general use cases and level of time commitment.    Workflow 1   Some words    My First Subsection  Some more words  foo     Workflow 2   Some words    "
+},
+{
+  "id": "echo360",
+  "level": "1",
+  "url": "echo360.html",
+  "type": "Section",
+  "number": "11",
+  "title": "Echo360",
+  "body": " Echo360   Remarks about general use cases and level of time commitment.    Workflow 1   Some words    My First Subsection  Some more words  foo     Workflow 2   Some words    "
 }
 ]
 
